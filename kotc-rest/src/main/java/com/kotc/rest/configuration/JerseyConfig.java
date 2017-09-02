fc545b7ad64246ecbdb943601b5cb050858cd5af
mway-dmueller <d.mueller@mwaysolutions.com>
2017-09-02 19:03:31 +0200

    Add ValidationExceptionMapper.

--- kotc-rest/src/main/java/com/kotc/rest/configuration/JerseyConfig.java
+++ kotc-rest/src/main/java/com/kotc/rest/configuration/JerseyConfig.java
@@ -3,6 +3,7 @@ package com.kotc.rest.configuration;
 import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.validation.internal.ValidationExceptionMapper;
 import org.springframework.stereotype.Component;
 
 import com.kotc.rest.resource.BasicResource;
@@ -12,6 +13,7 @@ import com.kotc.rest.resource.BasicResource;
 public class JerseyConfig extends ResourceConfig {
 
 	public JerseyConfig() {
+		register(ValidationExceptionMapper.class);
 		packages(BasicResource.PACKAGE_NAME);
 	}
 }
