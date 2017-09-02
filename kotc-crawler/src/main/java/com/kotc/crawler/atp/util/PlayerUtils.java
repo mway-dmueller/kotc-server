8cb33b10afe1310bc81010750ea8595388af53a1
mway-dmueller <d.mueller@mwaysolutions.com>
2017-09-02 21:21:58 +0200

    Fix PlayerUtils.extractPlayerCode() by decoding the url before using regular expressions.

--- kotc-crawler/src/main/java/com/kotc/crawler/atp/util/PlayerUtils.java
+++ kotc-crawler/src/main/java/com/kotc/crawler/atp/util/PlayerUtils.java
@@ -1,16 +1,23 @@
 package com.kotc.crawler.atp.util;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PlayerUtils {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(PlayerUtils.class);
 
 	/*
 	 * pattern: /en/players/<player-name>/<player-code>/overview
 	 */
-	private static final Pattern PLAYER_OVERVIEW_PATTERN = Pattern.compile("/en/players/([\\w\\-]+)/([\\w]+)/overview");
+	private static final Pattern PLAYER_OVERVIEW_PATTERN = Pattern.compile("/en/players/([^/]+)/([\\w]+)/overview");
 
 	private PlayerUtils() {
 		assert false : "not instantiable";
@@ -18,11 +25,17 @@ public class PlayerUtils {
 
 	public static final String extractPlayerCode(final String link) {
 
-		final Matcher matcher = PLAYER_OVERVIEW_PATTERN.matcher(link);
-		if (!matcher.find()) {
-			return null;
+		try {
+			final String url = URLDecoder.decode(link, StandardCharsets.UTF_8.name());
+			final Matcher matcher = PLAYER_OVERVIEW_PATTERN.matcher(url);
+			if (matcher.find()) {
+				return StringUtils.upperCase(matcher.group(2));
+			}
+		} catch (final UnsupportedEncodingException e) {
+			LOGGER.error(e.getMessage(), e);
 		}
 
-		return StringUtils.upperCase(matcher.group(2));
+		assert false : "code could not be extracted from " + link;
+		return null;
 	}
 }
